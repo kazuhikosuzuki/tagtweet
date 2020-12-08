d0728f6957138f867a304201da72d0571015958b
kazuhikosuzuki <72254144+kazuhikosuzuki@users.noreply.github.com>
2020-12-08 22:57:13 +0900

    fix_migration

--- db/migrate/20201208135610_create_tweets.rb
+++ db/migrate/20201208135610_create_tweets.rb
@@ -1,8 +1,8 @@
-class CreateTags < ActiveRecord::Migration[6.0]
+class CreateTweets < ActiveRecord::Migration[6.0]
   def change
-    create_table :tags do |t|
+    create_table :tweets do |t|
       t.string :message, null:false
- 
+
       t.timestamps
     end
   end
